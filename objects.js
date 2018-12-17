75aac4c0c0efaffa8fde997c1d73b1bc182044ca
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-17 14:02:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,10 @@
 var playlist = {}
 
-function updatePlaylist (playlist, artist, song)
+function updatePlaylist (playlist, artist, song){
+  playlist.artist = song
+  return playlist
+}
 
+function removeFromPlaylist (playlist, artist){
+  
+}
